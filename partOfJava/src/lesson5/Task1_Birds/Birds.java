d1abd7c5be9156873a8a798e6ff45cfb67335202
nadin <nadin.prakapovich@gmail.com>
2021-08-16 09:50:12 +0300

    Added class birds and methods

---
+++ partOfJava/src/lesson5/Task1_Birds/Birds.java
@@ -0,0 +1,38 @@
+package lesson5.Task1_Birds;
+
+import java.lang.System;
+
+public class Birds {
+    final String kind;
+    double weight;
+    static double allWeight;
+
+
+    // конструктор
+    public Birds(String kind, double weight) {
+        this.kind = kind;
+        this.weight = weight;
+    }
+
+    public Birds(String kind) {
+        this.kind = kind;
+        this.weight = 0;
+    }
+
+    public double setWight(double weight) {
+        return this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    static public void SumWeght(double weight) {
+        allWeight = allWeight + weight;
+        System.out.println(allWeight);
+    }
+}
