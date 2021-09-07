ea754618d096de47e4a252f1c9709f231176228a
nadin <nadin.prakapovich@gmail.com>
2021-09-07 20:57:20 +0300

    Fix lesson10

--- partOfJava/src/Lesson10/Main.java
+++ partOfJava/src/Lesson10/Main.java
@@ -3,46 +3,49 @@ package Lesson10;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        divideToWord();
+	public static void main(String[] args) {
+		divideToWord();
 
-        divideToSentences();
+		divideToSentences();
 
-        ArrayList<String> str = new ArrayList<>();
-        str.add("summer ");
-        str.add("autumn ");
-        str.add("winter");
-        str.add("spring");
+		ArrayList<String> str = new ArrayList<>();
+		str.add("summer");
+		str.add("autumn");
+		str.add("winter");
+		str.add("spring");
 
-        System.out.println(joinListString(str));
+		System.out.println(joinListString(str));
 
-    }
+	}
 
-    public static void divideToWord() {
-        String str = "Магнитное поле решает непреложный метеорит.";
-        String[] words = str.split(" ");
-        for (int i = 0; i < words.length; i++) {
-            System.out.println(words[i]);
-        }
-    }
+	public static void divideToWord() {
+		String str = "Магнитное поле решает непреложный метеорит.";
+		String[] words = str.split(" ");
+		for (int i = 0; i < words.length; i++) {
+			System.out.println(words[i]);
+		}
+	}
 
-    public static void divideToSentences() {
-        String str = "Магнитное поле решает непреложный метеорит. Маятник Фуко, несмотря на внешние воздействия, представляет собой эффективный диаметp! Надир решает космический узел?";
-        String[] arraySentence = str.split("[.|?|!]");
+	public static void divideToSentences() {
+		String str = "Магнитное поле решает непреложный метеорит. Маятник Фуко, несмотря на внешние воздействия, представляет собой эффективный диаметp! Надир решает космический узел?";
+		String[] arraySentence = str.split("[.|?|!]");
 
-        for (int s = 0; s < arraySentence.length; s++) {
-            System.out.println(arraySentence[s]);
-        }
-    }
+		for (int s = 0; s < arraySentence.length; s++) {
+			System.out.println(arraySentence[s].trim());
+		}
+	}
 
-    public static ArrayList<String> joinListString(ArrayList<String> str) {
-        ArrayList<String> newStr = new ArrayList<>();
-        for (int i = 0; i < str.size(); i++) {
-            newStr.add(str.get(i));
-        }
-        return newStr;
+	public static String joinListString(ArrayList<String> str) {
+		StringBuilder newStr = new StringBuilder();
+		for (int i = 0; i < str.size(); i++) {
+			newStr.append(str.get(i));
+			if (i != str.size() - 1) {
+				newStr.append(", ");
+			}
+		}
+		return newStr.toString();
 
-    }
+	}
 
 
 }
