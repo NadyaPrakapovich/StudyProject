6f64db1f53cb87f72904d9852e9ab42d1ee18cf4
BIOINFORMATICS\Nadin <nadinprakapovich@gmail.com>
2021-09-20 17:43:58 +0300

    Fix todo in generic class

--- partOfJava/src/lesson6/InOutSystem/Room.java
+++ partOfJava/src/lesson6/InOutSystem/Room.java
@@ -1,15 +1,12 @@
 package lesson6.InOutSystem;
 
-// TODO: 9/1/2021 With this realization I can create a room with String type.
-// Must be limited by Employees class [Pavel.Chachotkin]
-public class Room<T> {
-    T employee; // TODO: 9/1/2021 With this realization I can create cabinet onl for one person [Pavel.Chachotkin]
+public class Room<T extends Employee> {
+    T[] employees;
     private static int countEmployeeTheRoom;
-    private static int count = 1; // TODO: 9/1/2021 If I get count before adding employees which count I'll catch? [Pavel.Chachotkin]
 
-    public Room(T employee) {
-        this.employee = employee;
-        countEmployeeTheRoom = count++;
+      public Room(T[] employees) {
+        this.employees = employees;
+        countEmployeeTheRoom++;
     }
 
     public int getCountEmployeeTheRoom() {
@@ -17,6 +14,8 @@ public class Room<T> {
     }
 
     public void enterTheRoom() {
+
+
         System.out.println("Employee is the room ...");
     }
 }
