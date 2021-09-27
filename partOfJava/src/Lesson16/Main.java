2262e95f4cdbca47aa923a8d7157c550f91f5594
nadin <nadin.prakapovich@gmail.com>
2021-09-28 01:01:21 +0300

    Add  task3, lesson16

--- partOfJava/src/Lesson16/Main.java
+++ partOfJava/src/Lesson16/Main.java
@@ -2,6 +2,7 @@ package Lesson16;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -12,7 +13,7 @@ public class Main {
 		arrayList.add("series");
 		arrayList.add("world");
 		arrayList.add("dfdfgserjj");
-		arrayList.add("dg");
+		arrayList.add("dgv");
 
 		ArrayList<String> arrayList1 = task1(arrayList);
 		for (String s : arrayList1) {
@@ -22,12 +23,23 @@ public class Main {
 		System.out.println("--------------------");
 		System.out.println(task2(arrayList));
 
+		System.out.println("--------------------");
+
+		ArrayList<Phone> arrayList2 = new ArrayList<>();
+		arrayList2.add(new Phone("BB", 100));
+		arrayList2.add(new Phone("CCC", 200));
+		arrayList2.add(new Phone("A", 150));
+
+		getPrice(arrayList2);
+
 	}
 
 
 	public static ArrayList<String> task1(ArrayList<String> arrayList) {
-		ArrayList<String> arrlist = new ArrayList<>();
-		arrlist = (ArrayList<String>) arrayList.stream().filter(s -> !s.contains("ser")).sorted(Comparator.comparing(String::length)).collect(Collectors.toList());
+		ArrayList<String> arrlist = (ArrayList<String>) arrayList.stream()
+				.filter(s -> !s.contains("ser"))
+				.sorted(Comparator.comparing(String::length))
+				.collect(Collectors.toList());
 
 		return arrlist;
 	}
@@ -35,9 +47,30 @@ public class Main {
 
 	public static String task2(ArrayList<String> arrayList) {
 
-		String str = arrayList.stream().filter(s -> s.contains("vor")).findFirst().orElse("No elements");
+		String str = arrayList.stream()
+				.filter(s -> s.contains("vor"))
+				.findFirst().orElse("No elements");
 
 		return str;
 	}
+
+	// method not return ArrayList<Price>
+	public static ArrayList<Price> getPrice(ArrayList<Phone> phoneArrayList) {
+
+		ArrayList<Price> priceArrayList = new ArrayList<>();
+
+	 phoneArrayList.stream()
+				.sorted((p1, p2) -> {
+					int compareByName = p1.getName().length() - p2.getName().length();
+					if (compareByName == 0) {
+						return p2.getPrice() - p1.getPrice();
+					}
+					return compareByName;
+				})
+				.forEach(s-> System.out.println(s.getPrice()));
+
+		return priceArrayList;
+	}
+
 
 }
---
+++ partOfJava/src/Lesson16/Price.java
@@ -0,0 +1,17 @@
+package Lesson16;
+
+public class Price {
+	private int value;
+
+	public  Price(int value){
+		this.value=value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value){
+		this.value=value;
+	}
+}
