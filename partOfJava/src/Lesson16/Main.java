0224376e3fffed3fe5459179a296a64773f05a72
nadin <nadin.prakapovich@gmail.com>
2021-09-27 23:22:40 +0300

    Add  task1, lesson16

--- partOfJava/src/Lesson16/Main.java
+++ partOfJava/src/Lesson16/Main.java
@@ -1,40 +1,43 @@
 package Lesson16;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("service");
-        arrayList.add("vortex");
-        arrayList.add("series");
-        arrayList.add("world");
+	public static void main(String[] args) {
+		ArrayList<String> arrayList = new ArrayList<>();
+		arrayList.add("service");
+		arrayList.add("vortex");
+		arrayList.add("series");
+		arrayList.add("world");
+		arrayList.add("dfdfgserjj");
+		arrayList.add("dg");
 
-        task1(arrayList);
+		ArrayList<String> arrayList1 = task1(arrayList);
+		for (String s : arrayList1) {
+			System.out.println(s);
+		}
 
-       // System.out.println(task2(arrayList));
+		System.out.println("--------------------");
+		System.out.println(task2(arrayList));
 
-    }
-
-
-    public static ArrayList<String> task1(ArrayList<String> arrayList) {
-       // ArrayList<String> arrlist = new ArrayList<>();
-      arrayList.stream().filter(s -> s.contains("ser")).forEach(s -> System.out.println(s));
-       //  arrayList.stream().dropWhile(s -> s.contains("ser")).forEach(s -> System.out.println(s));
-
-
+	}
 
 
-        return arrayList;
-    }
+	public static ArrayList<String> task1(ArrayList<String> arrayList) {
+		ArrayList<String> arrlist = new ArrayList<>();
+		arrlist = (ArrayList<String>) arrayList.stream().filter(s -> !s.contains("ser")).sorted(Comparator.comparing(String::length)).collect(Collectors.toList());
+
+		return arrlist;
+	}
 
 
-    public static String task2(ArrayList<String> arrayList) {
+	public static String task2(ArrayList<String> arrayList) {
 
-        String str = arrayList.stream().filter(s -> s.contains("vor")).findFirst().orElse("No elements");
+		String str = arrayList.stream().filter(s -> s.contains("vor")).findFirst().orElse("No elements");
 
-        return str;
-    }
+		return str;
+	}
 
 }
